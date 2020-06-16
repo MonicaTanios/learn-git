3dbe89a511d9d90c6439bc64ebf6db7d6ffd59aa
MonicaTanios <monicatanios@gmail.com>
2020-06-16 18:24:22 +0200

    Add Theme  * Add Palette Class.  * Edit AlertDialog shape.

---
+++ lib/palette.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class Palette {
+  static Color primary = Color.fromRGBO(9, 28, 50, 1);
+  static Color secondary = Color.fromRGBO(41, 48, 69, 1);
+
+}
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,44 +1,48 @@
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:learngit/palette.dart';
 import 'package:learngit/screens/login_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-          padding: const EdgeInsets.all(32),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LoginScreen()));
-            },
-            child: Column(children: <Widget>[
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 8,
-              ),
-              Text(
-                'Welcome to Learn Git !',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,),
-              ),
-              Expanded(
-                child: FlareActor(
-                  'assets/coding.flr',
-                  animation: 'coding',
+    return Container(
+      color: Palette.primary,
+      child: Padding(
+            padding: const EdgeInsets.all(32),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+              child: Column(children: <Widget>[
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 8,
                 ),
-              ),
-              Text(
-                'Tap to continue ...',
-                style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 8,
-              ),
-            ]),
+                Text(
+                  'Welcome to Learn Git !',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,),
+                ),
+                Expanded(
+                  child: FlareActor(
+                    'assets/coding.flr',
+                    animation: 'coding',
+                  ),
+                ),
+                Text(
+                  'Tap to continue ...',
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 8,
+                ),
+              ]),
+            ),
           ),
-        );
+    );
   }
 }
